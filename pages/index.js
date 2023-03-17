0929ddfa49d3ba4c86f0f5266ab8980a8fed897c
pd9333 <pd9333@gmail.com>
2023-03-17 18:31:01 +0800

    feat: index page polished

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import Link from 'next/link';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
@@ -19,23 +21,18 @@ export default function Home({ allPostsData }) {
       <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-      <p>This is a paragraph.</p>
-      <p>
-      (This is a sample website - you’ll be building a site like this on{' '}
-       <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-    </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
       <h2 className={utilStyles.headingLg}>Blog</h2>
       <ul className={utilStyles.list}>
       {allPostsData.map(({ id, date, title }) => (
           <li className={utilStyles.listItem} key={id}>
-          {title}
-          <br />
-          {id}
-          <br />
-          {date}
-        </li>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
       ))}
     </ul>
       </section>
